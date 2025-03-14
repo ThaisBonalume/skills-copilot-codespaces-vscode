cf2de14fbe6e8b4a3aafc605f41d504a20fe7ff8
ThaisBonalume <148352970+ThaisBonalume@users.noreply.github.com>
2025-03-14 17:32:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+  var member = this;
+  member.skills = [
+    { name: 'HTML', level: 'advanced' },
+    { name: 'CSS', level: 'advanced' },
+    { name: 'JavaScript', level: 'advanced' },
+    { name: 'React', level: 'advanced' },
+    { name: 'Angular', level: 'advanced' },
+    { name: 'Node', level: 'advanced' },
+    { name: 'Python', level: 'intermediate' },
+    { name: 'Django', level: 'intermediate' },
+    { name: 'Java', level: 'intermediate' },
+    { name: 'Spring', level: 'intermediate' },
+    { name: 'C#', level: 'intermediate' },
+    { name: 'Unity', level: 'intermediate' },
+    { name: 'SQL', level: 'intermediate' },
+    { name: 'MongoDB', level: 'intermediate' },
+  ];
+}
